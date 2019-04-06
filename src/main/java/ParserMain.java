cf0901cc667357992b265e9d39a4d15b9696f831
un-install <elllipsse@gmail.com>
2019-04-06 19:59:47 +0300

    searchResponseUrlFactory+Refactor+Logging

--- src/main/java/ParserMain.java
+++ src/main/java/ParserMain.java
@@ -1,24 +1,18 @@
-import com.google.gson.Gson;
-import dto.ProductContainer;
 import dto.ProductResponse;
-import org.json.JSONException;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.List;
-
 import static utils.ProductParserUtils.*;
 
 //by Daniluko
 public class ParserMain {
-    public static void main(String[] args) throws IOException, JSONException {
-        List<ProductResponse> products = getPrdouctResponses(serchUrlFactory("jeans"));
-        productResponsesToXml(products);
-
-        // System.out.println(serchUrlFactory("jeans"));
-        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/test.json"));
-        writer.write(new Gson().toJson(new ProductContainer(products)));
+    public static void main(String[] args) throws Exception {
+        System.setProperty("webdriver.gecko.driver", args.length < 2 ? "" : args[1]);
+        try {
+            List<ProductResponse> products = getProductResponses(searchResultUrlFactory(args.length != 0 ? args[0] : "Kleider"));
+            productResponsesFlushXml(products);
+        }catch (IllegalStateException e){
+            System.out.println("webdriver.gecko.driver not seted. Set path to the second parameter");
+            return;
+        }
     }
 }
 
--- src/main/java/utils/ProductParserUtils.java
+++ src/main/java/utils/ProductParserUtils.java
@@ -3,31 +3,43 @@ package utils;
 import com.google.gson.Gson;
 import dto.ProductContainer;
 import dto.ProductResponse;
-import org.json.JSONException;
-import org.jsoup.Connection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.nodes.FormElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import java.io.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class ProductParserUtils {
+    private static final Logger LOG = LogManager.getLogger(ProductParserUtils.class);
+
 
     //by Yanetta and mkharkhu
-    public static void productResponsesToXml(List<ProductResponse> productsResponses) {
+    public static void productResponsesFlushXml(List<ProductResponse> productsResponses) {
         try {
+            LOG.debug("start productResponsesFlushXml, productResponses size =" + productsResponses.size());
+            //setup marshaller
             JAXBContext jaxbContext = JAXBContext.newInstance(ProductContainer.class);
             Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
             jaxbMarshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-            jaxbMarshaller.marshal(new ProductContainer(productsResponses), fileFactory("xml"));
 
-        } catch (JAXBException | IOException  e) {
+            //flash xml
+            jaxbMarshaller.marshal(new ProductContainer(productsResponses), fileFactory("xml"));
+            LOG.debug("end productResponsesFlushXml");
+        }
+        catch (JAXBException | IOException e) {
+            LOG.debug("productResponseFlushXml failed!!!");
             e.printStackTrace();
         }
     }
@@ -35,13 +47,15 @@ public class ProductParserUtils {
     //by un-install
     public static File fileFactory(String type) throws IOException {
         if (System.getProperty("os.name").contains("Windows")) {
-            return checkedCreate("C:\\aboutyou.de-parser\\output\\out." + type);
+            return checkedCreateFile("C:\\aboutyou.de-parser\\output\\out." + type);
         } else {
-            return checkedCreate("/etc/aboutyou.de-parser/output/out." + type);
+            return checkedCreateFile("/etc/aboutyou.de-parser/output/out." + type);
         }
     }
 
-    public static File checkedCreate(String filePath) throws IOException {
+    //by un-install
+    public static File checkedCreateFile(String filePath) throws IOException {
+        LOG.debug("start checkedCreateFile, filePath=" + filePath);
         File outFile = new File(filePath);
         if (!outFile.exists()) {
             File outDir = new File(outFile.getParent());
@@ -50,23 +64,25 @@ public class ProductParserUtils {
             }
             outFile.createNewFile();
         }
+        LOG.debug("end checkedCreateFile, outFile path=" + outFile.getPath());
         return outFile;
     }
 
     //by un-install
-    public static ProductResponse productHtmlParser(String productUrl) throws JSONException, IOException {
+    public static ProductResponse productHtmlParser(String productUrl) throws IOException {
+        LOG.debug("start productHtmlParser, productUrl=" + productUrl);
         //get html document
-        Document doc = Jsoup.connect(productUrl).get();
+        Document doc = Jsoup.connect(productUrl).followRedirects(false).get();
         ProductResponse productResponse;
 
-        //check json
-        List<Element> jsons = doc.getElementsByAttributeValue("type", "application/ld+json");
-        if (jsons.stream().anyMatch(json -> json.data().contains("\"@type\":\"Product\""))) {
+        //collecting jsons
+        List<Element> productJson = doc.getElementsByAttributeValue("type", "application/ld+json").stream().
+                filter(json -> json.data().contains("\"@type\":\"Product\"")).collect(Collectors.toList());
 
+        //check json
+        if (productJson.size() != 0) {
             //setting values to ProductResponse from json
-            String productJson = jsons.stream().
-                    filter(json -> json.data().contains("\"@type\":\"Product\"")).collect(Collectors.toList()).get(0).data();
-            productResponse = new Gson().fromJson(productJson, ProductResponse.class);
+            productResponse = new Gson().fromJson(productJson.get(0).data(), ProductResponse.class);
 
             //setting values to ProductResponse from html
             productResponse.setArticleNumber(doc.body().getElementsByClass("_articleNumber_1474d").get(0).text().replace("Artikel-Nr: ", ""));
@@ -75,43 +91,63 @@ public class ProductParserUtils {
         } else {
             productResponse = new ProductResponse();
         }
-
-        System.out.println(productResponse);
+        LOG.debug("end productHtmlParser, productResponse=" + productResponse);
         return productResponse;
     }
 
     //by un-install
-    public static List<String> getSerchResultUrls(String serchUrl) throws IOException {
+    public static List<String> getSearchResultUrls(String searchUrl) throws IOException {
+        LOG.debug("start getSearchResultUrls, searchUrl=" + searchUrl);
         //get html document
-        Document doc = Jsoup.connect(serchUrl).get();
+        Document doc = Jsoup.connect(searchUrl).followRedirects(false).get();
 
         //get hrefs to products
-        return doc.body().getElementsByAttributeValue("data-test-id", "ProductTileDefault")
+        List<String> productUrls = doc.body().getElementsByAttributeValue("data-test-id", "ProductTileDefault")
                 .stream().map(div -> "https://www.aboutyou.de" + div.getElementsByTag("a").get(0).attr("href")).collect(Collectors.toList());
+
+        LOG.debug("end getSearchResultUrls, productUrls size=" + productUrls.size());
+        return productUrls;
     }
 
     //by un-install
-    public static List<ProductResponse> getPrdouctResponses(String serchUrl) throws IOException, JSONException {
-        return getSerchResultUrls(serchUrl).stream().map(url -> {
-            try {
-                return productHtmlParser(url);
-            } catch (IOException | JSONException e) {
-                e.printStackTrace();
-                return new ProductResponse();
-            }
-        }).collect(Collectors.toList());
+    public static List<ProductResponse> getProductResponses(String searchUrl) throws IOException {
+        LOG.debug("start getProductResponses, searchUrl=" + searchUrl);
+        //map productUrl to productResponse
+        List<ProductResponse> productResponses = getSearchResultUrls(searchUrl).stream()
+                .map(url -> {
+                    try {
+                        return productHtmlParser(url);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        return new ProductResponse();
+                    }
+                }).collect(Collectors.toList());
+
+        LOG.debug("end getProductResponses, productResponses list size=" + productResponses.size());
+        return productResponses;
     }
 
     //by un-install
-    public static String serchUrlFactory(String keyword) throws IOException {
-        Document doc = Jsoup.connect("https://www.aboutyou.de").followRedirects(true).data("_textInput_f8463 _textInputActive_f8463", keyword).post();
+    public static String searchResultUrlFactory(String keyword) throws IOException, InterruptedException {
+        LOG.debug("start searchResultUrlFactory, keyword=" + keyword);
+        //load page
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.aboutyou.de/");
 
-        FormElement serchForm = doc.getElementsByClass("_searchBarForm_f8463").forms().get(0);
-        serchForm.val(keyword);
-        Connection conn = serchForm.submit();
-        conn.followRedirects(true);
+        //submitting keyword to search form
+        driver.findElement(By.className("_searchBarWrapper_f8463")).click();
+        driver.findElement(By.className("_textInput_f8463")).sendKeys(keyword, Keys.ENTER);
 
-        return "https://www.aboutyou.de/maenner/bekleidung/jeans?is_s=suggest&is_h=gz&term=jeans&category=20202";
+        //getting url after redirection
+        while(driver.getCurrentUrl().equals("https://www.aboutyou.de/dein-shop")){
+            TimeUnit.MILLISECONDS.sleep(100);
+        }
+
+        String searchUrl = driver.getCurrentUrl();
+        driver.close();
+
+        LOG.debug("end searchResultUrlFactory, searchUrl=" + searchUrl);
+        return searchUrl;
     }
 
 }
